1025b7c16667d7f5c9d320c16ccfe2510d36491c
Babetta <Babetta@DESKTOP-18QPVJ9>
2022-11-09 10:12:30 +0500

    Добавьте файлы проекта.

---
+++ DigitalClock/MauiProgram.cs
@@ -0,0 +1,18 @@
+﻿namespace DigitalClock;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("Minecraftchmc.ttf", "Minecraftchmc");
+			});
+
+		return builder.Build();
+	}
+}
+
